e98f29c298b209a0b028abd2717ccdc2470254dd
Jon Khaykin <jon@jonkhaykin.com>
2015-02-14 16:18:50 -0600

    fixing s3 url src

--- app/models/resupload.rb
+++ app/models/resupload.rb
@@ -5,7 +5,7 @@ belongs_to :user
 has_many :suggestions
 
 has_attached_file :document, :url => "/docs/:id/:basename.:extension", styles: {
-    square: '16x16#'
+    square: '16x16'
   }
 
 validates_attachment_content_type :document, :content_type => /\Aimage\/.*\Z/
